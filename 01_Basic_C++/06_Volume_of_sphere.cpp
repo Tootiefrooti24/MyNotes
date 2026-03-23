487b520d13876b24effc437869eaaa61c306b866
Tootiefrooti24 <pshrutz24@gmail.com>
2026-03-23 18:04:39 +0000

    Added two basic problems of volume and simple interest

---
+++ 01_Basic_C++/06_Volume_of_sphere.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+cout<<"Enter radius: ";
+int r;
+float pi;
+pi=3.14;
+cin>>r;
+cout<<"The Volume of Sphere is: "<<(4*pi*r*r*r)/3<<endl;
+return 0;
+}
