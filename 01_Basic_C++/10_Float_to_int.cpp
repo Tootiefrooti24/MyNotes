b6d8067846526999c4e11a49cdcbb7c4f757a713
Tootiefrooti24 <pshrutz24@gmail.com>
2026-03-23 18:15:53 +0000

    conversion of float to int basic example

---
+++ 01_Basic_C++/10_Float_to_int.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"Enter float input: ";
+    float n;
+    cin>>n;
+    int m=n;
+    cout<<"Integer part is: "<<m<<endl;
+    cout<<"The fractional part of n is: "<<n-m<<endl;
+    return 0;
+}
