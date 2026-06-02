d936c6254a34e1cd5846dfd9ca7617eedd493f76
Shruti Pandey <pshrutz24@gmail.com>
2026-06-02 22:43:51 +0530

    Finding a Number is even or odd

---
+++ 02_Conditionals/01_IF_Else/01_Even_or_Odd.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter number: ";
+    cin>>a;
+    if(a%2==0){
+        cout<<"Even"<<endl;
+    }
+    else{
+        cout<<"Odd"<<endl;
+    }
+    return 0;
+}
